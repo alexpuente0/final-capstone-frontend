7f36a9d143a75716d6e60af60af44b83a5ae2aec
Aldo Aníbal Colombo <44879977+acolombo1@users.noreply.github.com>
2022-09-19 19:08:50 -0300

    Avoid reservations undefined errors

--- src/pages/ReservationsPage.js
+++ src/pages/ReservationsPage.js
@@ -3,24 +3,43 @@ import { useSelector, shallowEqual } from 'react-redux';
 function ReservationsPage() {
   const reservations = useSelector((state) => state.reservations.reservations, shallowEqual);
   console.log(reservations);
-  return (
-    <>
-      <h1>Reservations Page</h1>
-      <ul className="reservtable">
-        <li className="listitem">
-          <span>Car</span>
-          <span>City</span>
-          <span>Date</span>
-        </li>
-        {reservations.map((reservation) => (
-          <li key={reservation.id} className="listitem">
-            <span>{reservation.item}</span>
-            <span>{reservation.city}</span>
-            <span>{reservation.date}</span>
+  let codefragment = null;
+  if (reservations === undefined || reservations === []) {
+    codefragment = (
+      <>
+        <h1>Reservations Page</h1>
+        <ul className="reservtable">
+          <li className="listitem">
+            <span>Car</span>
+            <span>City</span>
+            <span>Date</span>
           </li>
-        ))}
-      </ul>
-    </>
+        </ul>
+      </>
+    );
+  } else {
+    codefragment = (
+      <>
+        <h1>Reservations Page</h1>
+        <ul className="reservtable">
+          <li className="listitem">
+            <span>Car</span>
+            <span>City</span>
+            <span>Date</span>
+          </li>
+          {reservations.map((reservation) => (
+            <li key={reservation.id} className="listitem">
+              <span>{reservation.item_id}</span>
+              <span>{reservation.city}</span>
+              <span>{reservation.date}</span>
+            </li>
+          ))}
+        </ul>
+      </>
+    );
+  }
+  return (
+    codefragment
   );
 }
 
