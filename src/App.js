7b3494e3a736f0c63d023f2d3d1bd12c27fdfe2b
Roberto A. Baños Alvarez <roberto.banos.mv@gmail.com>
2022-09-21 04:52:36 -0500

    Apply PrivateRoute to ReservationsPage

--- src/App.js
+++ src/App.js
@@ -1,18 +1,25 @@
-import { Route, Routes } from 'react-router-dom';
+import {
+  Route, Routes, useLocation, useNavigate,
+} from 'react-router-dom';
 import './App.css';
 import Header from './components/Header';
+import PrivateRoute from './components/PrivateRoute';
+import history from './helpers/history';
 import HomePage from './pages/HomePage';
 import ItemDetailPage from './pages/ItemDetailPage';
 import LoginPage from './pages/LoginPage';
 import ReservationsPage from './pages/ReservationsPage';
 
 function App() {
+  history.navigate = useNavigate();
+  history.location = useLocation();
+
   return (
     <div className="App">
       <Header />
       <Routes>
         <Route path="/" element={<HomePage />} />
-        <Route path="/reservations" element={<ReservationsPage />} />
+        <Route path="/reservations" element={<PrivateRoute><ReservationsPage /></PrivateRoute>} />
         <Route path="/login" element={<LoginPage />} />
         <Route path="/items/:id" element={<ItemDetailPage />} />
         {/* <Route path="/reservations/add" element={<ReservationsAddPage />} />
