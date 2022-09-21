3e9abcfaa23ea8844c7ee7fbf4fe44cff111c071
Roberto A. Baños Alvarez <roberto.banos.mv@gmail.com>
2022-09-21 07:08:56 -0500

    Update HomePage

--- src/pages/HomePage.js
+++ src/pages/HomePage.js
@@ -1,5 +1,23 @@
+import { useEffect } from 'react';
+import { shallowEqual, useDispatch, useSelector } from 'react-redux';
+import CarList from '../components/CarList';
+import { getItems } from '../redux/item/itemReducer';
+
 function HomePage() {
-  return <h1>Home Page</h1>;
+  const dispatch = useDispatch();
+
+  useEffect(() => {
+    dispatch(getItems());
+  }, [dispatch]);
+
+  const cars = useSelector((state) => state.items.items, shallowEqual) || [];
+
+  return (
+    <>
+      <h1>Home Page</h1>
+      <CarList cars={cars} />
+    </>
+  );
 }
 
 export default HomePage;
