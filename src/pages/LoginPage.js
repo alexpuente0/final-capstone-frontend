586a80a0433f57a21102d68bade8ad84291c49ae
Aldo Aníbal Colombo <44879977+acolombo1@users.noreply.github.com>
2022-09-21 08:27:58 -0300

    Small fixes

--- src/pages/LoginPage.js
+++ src/pages/LoginPage.js
@@ -1,22 +1,47 @@
+/* eslint-disable react/destructuring-assignment */
+import React from 'react';
+import { login } from '../redux/auth/auth';
+import store from '../redux/configureStore';
 import './login.css';
 
-function LoginPage() {
-  return (
-    <div className="loginpage">
-      <h1>Login</h1>
-      <div className="centered">
-        <form className="myform" action="" method="POST">
-          <input className="formitem" type="text" id="name" name="name" placeholder="Name" required minLength="2" maxLength="30" />
-          <input className="formitem" type="password" id="password" name="password" placeholder="Password" required />
-          <div className="button-wrapper">
-            &nbsp;
-            <button className="sbutton" type="submit">Submit</button>
-            &nbsp;
-          </div>
-        </form>
+class LoginPage extends React.Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      fields: {},
+    };
+  }
+
+  handleChange(field, e) {
+    const { fields } = this.state;
+    fields[field] = e.target.value;
+    this.setState({ fields });
+  }
+
+  processForm = (e) => {
+    e.preventDefault();
+    store.dispatch(login(this.state.fields.email, this.state.fields.password));
+  };
+
+  render() {
+    return (
+      <div className="loginpage">
+        <h1>Login</h1>
+        <div className="centered">
+          <form className="myform" onSubmit={this.processForm.bind(this)}>
+            <input className="formitem" type="email" id="email" name="email" placeholder="Email" onChange={this.handleChange.bind(this, 'email')} value={this.state.fields.email} required minLength="5" maxLength="30" />
+            <input className="formitem" type="password" id="password" name="password" placeholder="Password" onChange={this.handleChange.bind(this, 'password')} value={this.state.fields.password} required minLength="5" maxLength="30" />
+            <div className="button-wrapper">
+              &nbsp;
+              <button className="sbutton" type="submit">Submit</button>
+              &nbsp;
+            </div>
+          </form>
+        </div>
       </div>
-    </div>
-  );
+    );
+  }
 }
 
 export default LoginPage;
