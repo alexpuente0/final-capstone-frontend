f7e3b8941d693746794cc0ed5ce7cd726a3ca69c
alexpuente0 <99830261+alexpuente0@users.noreply.github.com>
2022-09-21 16:19:24 -0500

    Add the missing functionality to delete button
    also, only logged in users are able to see it

--- src/pages/ItemDetailPage.js
+++ src/pages/ItemDetailPage.js
@@ -1,7 +1,7 @@
 import { useEffect } from 'react';
 import { shallowEqual, useDispatch, useSelector } from 'react-redux';
 import { useNavigate, useParams } from 'react-router-dom';
-import { getItemDetails } from '../redux/item/itemReducer';
+import { getItemDetails, removeItem } from '../redux/item/itemReducer';
 
 function ItemDetailPage() {
   const dispatch = useDispatch();
@@ -13,8 +13,33 @@ function ItemDetailPage() {
     dispatch(getItemDetails(id));
   }, [dispatch, id]);
 
+  const authUser = useSelector((state) => state.auth.user);
   const item = useSelector((state) => state.items.itemDetails, shallowEqual);
   let codefragment = null;
+  let buttonfragment = null;
+
+  function onDelete(id) {
+    return dispatch(removeItem(id))
+      .then(() => navigate('/'));
+  }
+
+  buttonfragment = !authUser ? (
+    <div>
+      <button type="button" onClick={() => navigate('/reservations')}>
+        Reserve this Car
+      </button>
+    </div>
+  ) : (
+    <div>
+      <button type="button" onClick={() => navigate('/reservations')}>
+        Reserve this Car
+      </button>
+      <button type="button" onClick={() => onDelete(item.id)}>
+        Delete
+      </button>
+    </div>
+  );
+
   codefragment = item === undefined || item === [] ? (
     <h1>No Items</h1>
   ) : (
@@ -29,13 +54,9 @@ function ItemDetailPage() {
       <p>
         Range:
         {item.range}
+        {item.active}
       </p>
-
-      <button type="button" onClick={() => navigate('/reservations')}>
-        Reserve this Car
-      </button>
-
-      <button type="button">Delete</button>
+      {buttonfragment}
     </>
   );
   return codefragment;
