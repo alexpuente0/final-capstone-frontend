4bbb1c1e0c016b0b6c10ac48e2f37d3a6f7f47b9
Roberto A. Baños Alvarez <roberto.banos.mv@gmail.com>
2022-09-21 04:53:10 -0500

    Remove Login from Header

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,16 +1,10 @@
 import { NavLink } from 'react-router-dom';
-import { currentUser, login, logout } from '../redux/auth/auth';
+import { currentUser, logout } from '../redux/auth/auth';
 import store from '../redux/configureStore';
 import { getItemDetails } from '../redux/item/ItemReducer';
 
 const doCurrent = () => {
   store.dispatch(currentUser());
-};
-
-const doLogin = () => {
-  const email = 'roberto@mail.com';
-  const password = 'valido';
-  store.dispatch(login(email, password));
 };
 
 const doLogout = () => {
@@ -26,8 +20,6 @@ const Header = () => (
     <nav>
       <h2>This is the header</h2>
       <br />
-      <NavLink onClick={doLogin} to="/">Login</NavLink>
-      &nbsp;
       <NavLink onClick={doCurrent} to="/">Current User</NavLink>
       &nbsp;
       <NavLink onClick={doLogout} to="/">Logout</NavLink>
