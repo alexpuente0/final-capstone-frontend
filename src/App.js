45b0b2d9877df12a6ecf227f1a0bff8b190d04f9
Roberto A. Baños Alvarez <roberto.banos.mv@gmail.com>
2022-09-21 04:40:12 -0500

    Move router from App.js to index.js

--- src/App.js
+++ src/App.js
@@ -1,27 +1,25 @@
-import { HashRouter as Router, Route, Routes } from 'react-router-dom';
+import { Route, Routes } from 'react-router-dom';
 import './App.css';
 import Header from './components/Header';
 import HomePage from './pages/HomePage';
-import ReservationsPage from './pages/ReservationsPage';
-import LoginPage from './pages/LoginPage';
 import ItemDetailPage from './pages/ItemDetailPage';
+import LoginPage from './pages/LoginPage';
+import ReservationsPage from './pages/ReservationsPage';
 
 function App() {
   return (
-    <Router>
-      <div className="App">
-        <Header />
-        <Routes>
-          <Route path="/" element={<HomePage />} />
-          <Route path="/reservations" element={<ReservationsPage />} />
-          <Route path="/login" element={<LoginPage />} />
-          <Route path="/items/:id" element={<ItemDetailPage />} />
-          {/* <Route path="/reservations/add" element={<ReservationsAddPage />} />
-          <Route path="/" element={<ItemsAddPage />} />
-          <Route path="/" element={<ItemsDeletePage />} /> */}
-        </Routes>
-      </div>
-    </Router>
+    <div className="App">
+      <Header />
+      <Routes>
+        <Route path="/" element={<HomePage />} />
+        <Route path="/reservations" element={<ReservationsPage />} />
+        <Route path="/login" element={<LoginPage />} />
+        <Route path="/items/:id" element={<ItemDetailPage />} />
+        {/* <Route path="/reservations/add" element={<ReservationsAddPage />} />
+        <Route path="/" element={<ItemsAddPage />} />
+        <Route path="/" element={<ItemsDeletePage />} /> */}
+      </Routes>
+    </div>
   );
 }
 
