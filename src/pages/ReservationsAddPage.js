a4f15e89b6226f5918cf86a754181c14919f681e
Aldo Aníbal Colombo <44879977+acolombo1@users.noreply.github.com>
2022-09-22 18:45:40 -0300

    Redirect after submit reservation

--- src/pages/ReservationsAddPage.js
+++ src/pages/ReservationsAddPage.js
@@ -1,9 +1,10 @@
 /* eslint-disable react/jsx-props-no-spreading */
-import { useEffect } from 'react';
+import { useEffect, useState } from 'react';
 import { useForm } from 'react-hook-form';
 import { useDispatch, shallowEqual, useSelector } from 'react-redux';
 import { getItems } from '../redux/item/itemReducer';
 import { addReservation } from '../redux/reserv/reserv';
+import history from '../helpers/history';
 import './addReserv.css';
 
 function ReservationsAddPage() {
@@ -11,10 +12,19 @@ function ReservationsAddPage() {
   useEffect(() => {
     dispatch(getItems());
   }, [dispatch]);
+  const [redirect, setRedirect] = useState(false);
+  useEffect(() => {
+    // redirect to reservations after successful submit
+    if (redirect) {
+      history.navigate('/reservations');
+    }
+  }, [redirect]);
+
   const cars = useSelector((state) => state.items.items, shallowEqual) || [];
   const { register, handleSubmit } = useForm();
 
   function onSubmit({ item, city, date }) {
+    setRedirect(true);
     return dispatch(addReservation(item, city, date));
   }
 
