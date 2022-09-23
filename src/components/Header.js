b3adee94fece13679d60377d6f6a7e4b8e7ab166
alexpuente0 <99830261+alexpuente0@users.noreply.github.com>
2022-09-23 14:37:44 -0500

    Add conditional to header
    displays login and signup if the user isn't logged in. displays log out if user is logged in

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,4 +1,5 @@
 import { NavLink } from 'react-router-dom';
+import { useSelector } from 'react-redux';
 import { currentUser, logout } from '../redux/auth/auth';
 import store from '../redux/configureStore';
 
@@ -10,36 +11,45 @@ const doLogout = () => {
   store.dispatch(logout());
 };
 
-const Header = () => (
-  <header>
-    <nav>
-      <h2>This is the header</h2>
-      <br />
-      <NavLink onClick={doCurrent} to="/">
-        Home
-      </NavLink>
-      &nbsp;
-      <NavLink to="/reservations">Reservations</NavLink>
-      &nbsp;
-      <NavLink to="/reservations/add">New Reservation</NavLink>
-      &nbsp;
-      <NavLink to="/delete">Delete a Green</NavLink>
-      &nbsp;
-      <NavLink to="/new">Add a New Car</NavLink>
-      &nbsp;
-      <NavLink to="/login">Login</NavLink>
-      &nbsp;
-      <NavLink to="/signup">Signup</NavLink>
-      &nbsp;
-      <NavLink onClick={doLogout} to="/">
-        Logout
-      </NavLink>
-      <br />
-      <br />
-      <hr />
-      <br />
-    </nav>
-  </header>
-);
+const Header = () => {
+  const authUser = useSelector((state) => state.auth.user);
+
+  return (
+    <header>
+      <nav>
+        <h2>This is the header</h2>
+        <br />
+        <NavLink onClick={doCurrent} to="/">
+          Home
+        </NavLink>
+        &nbsp;
+        <NavLink to="/reservations">Reservations</NavLink>
+        &nbsp;
+        <NavLink to="/reservations/add">New Reservation</NavLink>
+        &nbsp;
+        <NavLink to="/delete">Delete a Green</NavLink>
+        &nbsp;
+        <NavLink to="/new">Add a New Car</NavLink>
+        &nbsp;
+        {authUser ? (
+          <NavLink onClick={doLogout} to="/">
+            Logout
+          </NavLink>
+
+        ) : (
+          <>
+            <NavLink to="/login">Login</NavLink>
+            &nbsp; &nbsp;
+            <NavLink to="/signup">Sign Up</NavLink>
+          </>
+        )}
+        <br />
+        <br />
+        <hr />
+        <br />
+      </nav>
+    </header>
+  );
+};
 
 export default Header;
