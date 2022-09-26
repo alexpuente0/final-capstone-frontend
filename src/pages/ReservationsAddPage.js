96949ceb6bb887dfa5cd29eac45c0d599a3df459
Aldo Aníbal Colombo <44879977+acolombo1@users.noreply.github.com>
2022-09-26 13:11:56 -0300

    Use useNavigate, modify tests to login first

--- src/pages/ReservationsAddPage.js
+++ src/pages/ReservationsAddPage.js
@@ -2,16 +2,15 @@
 import { useEffect, useState } from 'react';
 import { useForm } from 'react-hook-form';
 import { useDispatch, shallowEqual, useSelector } from 'react-redux';
-import { useParams } from 'react-router-dom';
+import { useNavigate, useParams } from 'react-router-dom';
 import { getItems } from '../redux/item/itemReducer';
 import { addReservation } from '../redux/reserv/reserv';
-import history from '../helpers/history';
 import './addReserv.css';
 
 function ReservationsAddPage() {
   const dispatch = useDispatch();
   const { id } = useParams();
-
+  const navigate = useNavigate();
   useEffect(() => {
     dispatch(getItems());
   }, [dispatch]);
@@ -20,9 +19,9 @@ function ReservationsAddPage() {
   useEffect(() => {
     // redirect to reservations after successful submit
     if (redirect) {
-      history.navigate('/reservations');
+      navigate('/reservations');
     }
-  }, [redirect]);
+  }, [navigate, redirect]);
 
   const cars = useSelector(
     (state) => state.items.items.filter((item) => (!id || item.id.toString() === id)), shallowEqual,
--- src/tests/interaction.test.js
+++ src/tests/interaction.test.js
@@ -15,7 +15,33 @@ import ReservationsAddPage from '../pages/ReservationsAddPage';
 import PrivateRoute from '../components/PrivateRoute';
 
 describe('Navigation', () => {
-  test('reservations', async () => {
+  test('login', async () => {
+    render(
+      <Provider store={configureStore}>
+        <BrowserRouter>
+          <Routes>
+            <Route path="/" element={<PrivateRoute><ReservationsAddPage /></PrivateRoute>} />
+            <Route path="/login" element={<LoginPage />} />
+            <Route path="/reservations" element={<ReservationsPage />} />
+            <Route path="/reservations/add" element={<PrivateRoute><ReservationsAddPage /></PrivateRoute>} />
+          </Routes>
+        </BrowserRouter>
+      </Provider>,
+    );
+
+    expect(screen.getByRole('heading')).toHaveTextContent('Login');
+    const email = screen.getByPlaceholderText('Email');
+    await userEvent.type(email, 'aldo@mail.com');
+    const password = screen.getByPlaceholderText('Password');
+    await userEvent.type(password, 'valido');
+    act(() => {
+      fireEvent.click(screen.getByText('Submit'));
+    });
+  });
+});
+
+describe('Navigation', () => {
+  test('add reservation', async () => {
     render(
       <Provider store={configureStore}>
         <MemoryRouter>
@@ -40,47 +66,5 @@ describe('Navigation', () => {
     await waitFor(() => {
       expect(screen.getByRole('heading')).toHaveTextContent('Reservations Page');
     });
-  });
-});
-
-describe('Navigation', () => {
-  test('add reservation', async () => {
-    render(
-      <Provider store={configureStore}>
-        <BrowserRouter>
-          <Routes>
-            <Route path="/" element={<PrivateRoute><ReservationsAddPage /></PrivateRoute>} />
-            <Route path="/login" element={<LoginPage />} />
-            <Route path="/reservations" element={<ReservationsPage />} />
-            <Route path="/reservations/add" element={<PrivateRoute><ReservationsAddPage /></PrivateRoute>} />
-          </Routes>
-        </BrowserRouter>
-      </Provider>,
-    );
-
-    expect(screen.getByRole('heading')).toHaveTextContent('Login');
-    const email = screen.getByPlaceholderText('Email');
-    await userEvent.type(email, 'aldo@mail.com');
-    const password = screen.getByPlaceholderText('Password');
-    await userEvent.type(password, 'valido');
-    act(() => {
-      fireEvent.click(screen.getByText('Submit'));
-    });
-
-    await waitFor(() => {
-      expect(screen.getByRole('heading')).toHaveTextContent('Add Reservation');
-    });
-    const car = screen.getByTestId('item');
-    await fireEvent.change(car, { target: { value: 'Genesis GV60' } });
-    const city = screen.getByPlaceholderText('City');
-    await userEvent.type(city, 'Monte Chingolo');
-    const date = screen.getByPlaceholderText('Date');
-    await userEvent.type(date, '01/02/2026');
-    act(() => {
-      fireEvent.click(screen.getByText('Submit'));
-    });
-
-    expect(screen.getByRole('heading')).toHaveTextContent('Reservations Page');
-    expect(screen.getAllByText('Genesis GV60')).toHaveLength(1);
   });
 });
