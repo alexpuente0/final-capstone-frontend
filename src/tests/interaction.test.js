e6291696b7657e2f28a02fa15fd30d4733abc8c8
Aldo Aníbal Colombo <44879977+acolombo1@users.noreply.github.com>
2022-09-26 10:09:57 -0300

    Try add reservation test

--- src/tests/interaction.test.js
+++ src/tests/interaction.test.js
@@ -4,16 +4,44 @@ import {
 } from '@testing-library/react';
 import { act } from 'react-dom/test-utils';
 import userEvent from '@testing-library/user-event';
-// eslint-disable-next-line no-unused-vars
-import { toHaveTextContent } from '@testing-library/jest-dom';
+import '@testing-library/jest-dom';
 import {
-  BrowserRouter, Route, Routes,
+  BrowserRouter, MemoryRouter, Route, Routes,
 } from 'react-router-dom';
 import configureStore from '../redux/configureStore';
 import LoginPage from '../pages/LoginPage';
 import ReservationsPage from '../pages/ReservationsPage';
 import ReservationsAddPage from '../pages/ReservationsAddPage';
 import PrivateRoute from '../components/PrivateRoute';
+
+describe('Navigation', () => {
+  test('reservations', async () => {
+    render(
+      <Provider store={configureStore}>
+        <MemoryRouter>
+          <Routes>
+            <Route path="/" element={<ReservationsAddPage />} />
+            <Route path="/reservations" element={<ReservationsPage />} />
+          </Routes>
+        </MemoryRouter>
+      </Provider>,
+    );
+
+    expect(screen.getByRole('heading')).toHaveTextContent('Add Reservation');
+    const car = screen.getByTestId('item');
+    await fireEvent.change(car, { target: { value: 'Genesis GV60' } });
+    const city = screen.getByPlaceholderText('City');
+    await userEvent.type(city, 'Monte Chingolo');
+    const date = screen.getByPlaceholderText('Date');
+    await userEvent.type(date, '01/02/2026');
+    act(() => {
+      fireEvent.click(screen.getByText('Submit'));
+    });
+    await waitFor(() => {
+      expect(screen.getByRole('heading')).toHaveTextContent('Reservations Page');
+    });
+  });
+});
 
 describe('Navigation', () => {
   test('add reservation', async () => {
@@ -51,7 +79,7 @@ describe('Navigation', () => {
     act(() => {
       fireEvent.click(screen.getByText('Submit'));
     });
-    await new Promise((r) => setTimeout(r, 2000));
+
     expect(screen.getByRole('heading')).toHaveTextContent('Reservations Page');
     expect(screen.getAllByText('Genesis GV60')).toHaveLength(1);
   });
