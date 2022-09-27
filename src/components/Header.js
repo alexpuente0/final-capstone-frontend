5fa99abf56498ced84bfab1f392a997b9f59de90
alexpuente0 <99830261+alexpuente0@users.noreply.github.com>
2022-09-26 21:07:06 -0500

    create hamburger button for mobile version

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,3 +1,4 @@
+import React from 'react';
 import { NavLink } from 'react-router-dom';
 import { useSelector } from 'react-redux';
 import { currentUser, logout } from '../redux/auth/auth';
@@ -14,27 +15,52 @@ const doLogout = () => {
 
 const Header = () => {
   const authUser = useSelector((state) => state.auth.user);
+  const [navbar, setNavbar] = React.useState('invisible');
+
+  const handleHamburgerClick = () => {
+    setNavbar(navbar === 'invisible' ? '' : 'invisible');
+  };
 
   return (
     <header>
-      <nav>
-        <ul className="navbar">
+      <div
+        className="hamburger"
+        id="hamburger"
+        role="presentation"
+        onClick={handleHamburgerClick}
+      >
+        <div className="slice" />
+
+        <div className="slice" />
+
+        <div className="slice" />
+      </div>
+      <nav className={navbar}>
+        <ul className="navigator">
           <li>
             <NavLink onClick={doCurrent} to="/">
               Home
             </NavLink>
           </li>
           <li>
-            <NavLink to="/reservations">My Reservations</NavLink>
+            <NavLink onClick={handleHamburgerClick} to="/reservations">
+              My Reservations
+            </NavLink>
           </li>
           <li>
-            <NavLink to="/reservations/add">Reserve Green</NavLink>
+            <NavLink onClick={handleHamburgerClick} to="/reservations/add">
+              Reserve Green
+            </NavLink>
           </li>
           <li>
-            <NavLink to="/new">New Green</NavLink>
+            <NavLink onClick={handleHamburgerClick} to="/new">
+              New Green
+            </NavLink>
           </li>
           <li>
-            <NavLink to="/delete">Delete Green</NavLink>
+            <NavLink onClick={handleHamburgerClick} to="/delete">
+              Delete Green
+            </NavLink>
           </li>
           {authUser ? (
             <li>
@@ -45,10 +71,14 @@ const Header = () => {
           ) : (
             <>
               <li>
-                <NavLink to="/login">Log In</NavLink>
+                <NavLink onClick={handleHamburgerClick} to="/login">
+                  Log In
+                </NavLink>
               </li>
               <li>
-                <NavLink to="/signup">Sign Up</NavLink>
+                <NavLink onClick={handleHamburgerClick} to="/signup">
+                  Sign Up
+                </NavLink>
               </li>
             </>
           )}
