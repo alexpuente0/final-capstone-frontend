47b5e2035f7fc3dc9425c8b8eaf387ad7497067f
alexpuente0 <99830261+alexpuente0@users.noreply.github.com>
2022-09-27 18:38:35 -0500

    Update reservation_add.test.js

--- src/tests/reservation_add.test.js
+++ src/tests/reservation_add.test.js
@@ -1,7 +1,5 @@
 import '@testing-library/jest-dom';
-import {
-  fireEvent, render, screen, waitFor,
-} from '@testing-library/react';
+import { fireEvent, render, screen, waitFor } from '@testing-library/react';
 import { Provider } from 'react-redux';
 import { MemoryRouter as Router } from 'react-router-dom';
 import App from '../App';
